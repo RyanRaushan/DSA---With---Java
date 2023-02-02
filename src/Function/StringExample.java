3267afb502103cfe9be27147a5932c09c2947cc0
Raushan Ryan <raushankrpnhs2000@gmail.com>
2023-02-03 02:03:00 +0530

    DSA - DAY - 03 - Function

---
+++ src/Function/StringExample.java
@@ -0,0 +1,29 @@
+package Function;
+
+import java.util.Scanner;
+
+public class StringExample {
+
+    public static void main(String[] args) {
+       String message = greet();
+       System.out.println(message);
+
+        Scanner input  = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String naam = input.next();
+        String personalised = myGreet(naam);
+        System.out.println(personalised);
+        input.close();
+    }
+
+    static String myGreet(String name) {
+        String message = "Hello " + name;
+        return message;
+    }
+
+
+    static String greet() {
+        String greeting = "how are you";
+        return greeting;
+    }
+}
---
+++ src/Function/Sum.java
@@ -0,0 +1,41 @@
+package Function;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        int ans = sum2();
+        System.out.println(ans);
+
+        ans = sum3(20, 30);
+        System.out.println(ans);
+    }
+
+    /*
+     * return_type name (arguments) {
+     * // body
+     * return statement;
+     * }
+     * 
+     */
+
+    // pass the value of numbers when you are calling the method in main()
+    static int sum3(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // return the value
+    static int sum2() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = input.nextInt();
+        input.close();
+        int sum = num1 + num2;
+        return sum;
+        // System.out.println("This will never execute");
+    }
+
+}
